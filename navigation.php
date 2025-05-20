17dfd3a982d26798c3600ad04f33e0f6a308eac9
luisantoniojopia <luis_antonio_jopia@dlsl.edu.ph>
2025-05-20 21:40:21 +0800

    Update navigation

---
+++ navigation.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="navigation">
+        <ul>
+            <li><a href="index.php?page=dashboard" class="<?= $currentPage === 'dashboard' ? 'active-page' : '' ?>">Dashboard</a></li>
+            <li><a href="index.php?page=revenue" class="<?= $currentPage === 'revenue' ? 'active-page' : '' ?>">Revenue</a></li>
+            <li><a href="index.php?page=expense" class="<?= $currentPage === 'expense' ? 'active-page' : '' ?>">Expense</a></li>
+            <li><a href="index.php?page=profit" class="<?= $currentPage === 'profit' ? 'active-page' : '' ?>">Profit</a></li>
+        </ul>
+    </div>
+</div>
