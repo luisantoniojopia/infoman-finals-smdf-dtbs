e2c1b7f6943875f302f80fb3f3b048d29f084c47
luisantoniojopia <luis_antonio_jopia@dlsl.edu.ph>
2025-05-25 23:04:53 +0800

    Create base navigation

--- index.php
+++ index.php
@@ -7,26 +7,29 @@
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-
+<div class="wrapper d-flex">
     <?php
+    $currentPage = $_GET['page'] ?? ''; // Needed for navigation highlighting
     include('navigation.php');
-
-    $pages = [
-        'dashboard' => 'dashboard.php',
-        'revenue' => 'revenue/revenue.php',
-        'expense' => 'expense/expense.php',
-        'profit' => 'profit/profit.php'
-    ];
-
-    echo '<div class="content">';
-    if (isset($_GET['page']) && isset($pages[$_GET['page']])) {
-        include $pages[$_GET['page']];
-    } else {
-        echo "<p>Welcome to SMDF Database. Use the menu to navigate. </p>";
-    }
-
-    echo '</div>';
     ?>
+
+    <div class="content p-4 flex-grow-1">
+        <?php
+        $pages = [
+            'dashboard' => 'dashboard.php',
+            'revenue' => 'revenue/revenue.php',
+            'expense' => 'expense/expense.php',
+            'profit' => 'profit/profit.php'
+        ];
+
+        if (isset($_GET['page']) && isset($pages[$_GET['page']])) {
+            include $pages[$_GET['page']];
+        } else {
+            echo "<p>Welcome to SMDF Database. Use the menu to navigate. </p>";
+        }
+        ?>
+    </div>
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 
