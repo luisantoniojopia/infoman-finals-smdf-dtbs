7799de59f4eb9eaa17a716d8d7a15306d75a454f
luisantoniojopia <luis_antonio_jopia@dlsl.edu.ph>
2025-05-25 16:46:44 +0800

    Include bootstrap via CDN

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SMDF Accounting System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</head>
+<body>
+
 <?php
 include('navigation.php');
 
@@ -17,3 +27,6 @@ if (isset($_GET['page']) && isset($pages[$_GET['page']])) {
 
 echo '</div>';
 ?>
+
+</body>
+</html>
