9d19f0717b47df9ae2c7a2363a36daf8fc4bb5e1
luisantoniojopia <luis_antonio_jopia@dlsl.edu.ph>
2025-05-26 08:08:29 +0800

    Fix sidebar

--- navigation.php
+++ navigation.php
@@ -1,10 +1,8 @@
-<div class="container">
-    <nav class="sidebar">
-        <ul>
-            <li><a href="index.php?page=dashboard" class="<?= $currentPage === 'dashboard' ? 'active-page' : '' ?>">Dashboard</a></li>
-            <li><a href="index.php?page=revenue" class="<?= $currentPage === 'revenue' ? 'active-page' : '' ?>">Revenue</a></li>
-            <li><a href="index.php?page=expense" class="<?= $currentPage === 'expense' ? 'active-page' : '' ?>">Expense</a></li>
-            <li><a href="index.php?page=profit" class="<?= $currentPage === 'profit' ? 'active-page' : '' ?>">Profit</a></li>
-        </ul>
-    </nav>
-</div>
+<nav class="sidebar">
+    <ul>
+        <li><a href="index.php?page=dashboard" class="<?= $currentPage === 'dashboard' ? 'active-page' : '' ?>">Dashboard</a></li>
+        <li><a href="index.php?page=revenue" class="<?= $currentPage === 'revenue' ? 'active-page' : '' ?>">Revenue</a></li>
+        <li><a href="index.php?page=expense" class="<?= $currentPage === 'expense' ? 'active-page' : '' ?>">Expense</a></li>
+        <li><a href="index.php?page=profit" class="<?= $currentPage === 'profit' ? 'active-page' : '' ?>">Profit</a></li>
+    </ul>
+</nav>
